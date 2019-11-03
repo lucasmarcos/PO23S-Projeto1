ad25f111f6acd14b49a2df59b92f7f2f73ce6e6b
Lucas Marcos <lucasmarcos@live.com>
2019-11-03 15:18:02 -0300

    refatorando

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,17 +4,182 @@ import dao.DAOConta;
 import dao.DAOPessoa;
 import entidades.Conta;
 import entidades.Pessoa;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class Main {
 	private static Scanner scanner;
+	private static DAOConta daoConta;
+	private static DAOPessoa daoPessoa;
+
+	private static void inserirPessoa() {
+		Pessoa pessoa = new Pessoa();
+				
+		System.out.print("Nome: ");
+		pessoa.setNome(scanner.next());
+
+		System.out.print("CPF: ");
+		pessoa.setCpf(scanner.next());
+
+		System.out.print("Idade: ");
+		pessoa.setIdade(scanner.nextInt());
+
+		System.out.print("Cidade: ");
+		pessoa.setCidade(scanner.next());
+		
+		daoPessoa.inserir(pessoa);
+		pessoa.mostrar();
+	}
+
+	private static void inserirConta() {
+		Conta conta = new Conta();
+				
+		System.out.print("Banco: ");
+		conta.setBanco(scanner.next());
+
+		System.out.print("Numero: ");
+		conta.setNumero(scanner.nextInt());
+
+		System.out.print("Saldo: ");
+		conta.setSaldo(scanner.nextDouble());
+
+		System.out.print("Pessoa: ");
+		conta.setPessoa(daoPessoa.buscarPessoaPorId(scanner.nextInt()));
+		
+		daoConta.inserir(conta);
+		conta.mostrar();
+	}
+
+	private static void atualizarPessoa() {
+		System.out.println("modificar Pessoa com id: ");
+		Pessoa p = daoPessoa.buscarPessoaPorId(scanner.nextInt());
+
+		System.out.print("Nome [" + p.getNome() + "]: ");
+		String novoNome = scanner.next();
+		if(!novoNome.isEmpty()) {
+			System.out.println(novoNome);
+			p.setNome(novoNome);
+		}
+
+		System.out.print("CPF [" + p.getCpf() + "]: ");
+		String novoCPF = scanner.next();
+		if(!novoCPF.isEmpty()) {
+			System.out.println(novoCPF);
+			p.setCpf(novoCPF);
+		}
+
+		System.out.print("Idade [" + p.getIdade() + "]: ");
+		String novaIdade = scanner.next();
+		if(!novaIdade.isEmpty()) {
+			System.out.println(novaIdade);
+			p.setIdade(Integer.parseInt(novaIdade));
+		}
+
+		System.out.print("Cidade [" + p.getCidade() + "]: ");
+		String novaCidade = scanner.next();
+		if(!novaCidade.isEmpty()) {
+			System.out.println(novaCidade);
+			p.setCidade(novaCidade);
+		}
+
+		daoPessoa.atualizarPessoa(p);
+		p = daoPessoa.buscarPessoaPorId(p.getId());
+		p.mostrar();
+	}
+
+	private static void atualizarConta() {
+		System.out.println("modificar Conta com id: ");
+		Conta c = daoConta.buscarContaPorId(scanner.nextInt());
+
+		System.out.print("Banco [" + c.getBanco() + "]: ");
+		String novoBanco = scanner.next();
+		if(!novoBanco.isEmpty()) {
+			System.out.println(novoBanco);
+			c.setBanco(novoBanco);
+		}
+
+		System.out.print("Numero [" + c.getNumero() + "]: ");
+		String novoNumero = scanner.next();
+		if(!novoNumero.isEmpty()) {
+			System.out.println(novoNumero);
+			c.setNumero(Integer.getInteger(novoNumero));
+		}
+
+		System.out.print("Saldo [" + c.getSaldo() + "]: ");
+		String novoSaldo = scanner.next();
+		if(!novoSaldo.isEmpty()) {
+			System.out.println(novoSaldo);
+			c.setSaldo(Double.parseDouble(novoSaldo));
+		}
+
+		System.out.print("Pessoa [ " + c.getPessoa().getId() +"]: ");
+		String novaPessoa = scanner.next();
+		if(!novaPessoa.isEmpty()) {
+			System.out.println(novaPessoa);
+			c.setPessoa(daoPessoa.buscarPessoaPorId(Integer.parseInt(novaPessoa)));
+		}
+
+		daoConta.atualizarConta(c);
+		c = daoConta.buscarContaPorId(c.getId());
+		c.mostrar();
+	}
+
+	private static void pesquisarPessoa() {
+		List<Pessoa> pessoas;
+
+		System.out.println("pesquisar Pessoa por: ");
+		System.out.println("[ 1 ] Nome, [ 2 ] Cidade");
+		int filto_pessoa = scanner.nextInt();
+		switch(filto_pessoa) {
+		case 1:
+			pessoas = daoPessoa.pesquisar("nome", scanner.next());
+			break;
+
+		case 2:
+			pessoas = daoPessoa.pesquisar("cidade", scanner.next());
+			break;
+		
+		default:
+			pessoas = new ArrayList<Pessoa>();
+			break;
+		}
+
+		for(int i = 0; i < pessoas.size(); i++)
+			pessoas.get(i).mostrar();
+	}
+
+	private static void pesquisarConta() {
+		List<Conta> contas;
+
+		System.out.println("pesquisar Conta por: ");
+		System.out.println("[ 1 ] Banco, [ 2 ] ID_Pessoa");
+		int filto_conta = scanner.nextInt();
+		switch(filto_conta) {
+		case 1:
+			contas = daoConta.pesquisar("banco", scanner.next());
+			break;
+
+		case 2:
+			contas = daoConta.pesquisar("pessoa", scanner.next());
+			break;
+		
+		default:
+			contas = new ArrayList<Conta>();
+			break;
+		}
+
+		for(int i = 0; i < contas.size(); i++)
+			contas.get(i).mostrar();
+	}
 
 	public static void main(String[] args) {
-		DAOConta daoConta = new DAOConta();
-		DAOPessoa daoPessoa = new DAOPessoa();
-
+		daoConta = new DAOConta();
+		daoPessoa = new DAOPessoa();
 		scanner = new Scanner(System.in);
+		
 		// scanner.useDelimiter("\\n");
 
 		boolean continuar = true;
@@ -40,41 +205,11 @@ public class Main {
 			int opcao = scanner.nextInt();
 			switch (opcao) {
 			case 1:
-				Pessoa pessoa = new Pessoa();
-				
-				System.out.print("Nome: ");
-				pessoa.setNome(scanner.next());
-
-				System.out.print("CPF: ");
-				pessoa.setCpf(scanner.next());
-
-				System.out.print("Idade: ");
-				pessoa.setIdade(scanner.nextInt());
-
-				System.out.print("Cidade: ");
-				pessoa.setCidade(scanner.next());
-				
-				daoPessoa.inserir(pessoa);
-				pessoa.mostrar();
+				inserirPessoa();
 				break;
 
 			case 2:
-				Conta conta = new Conta();
-				
-				System.out.print("Banco: ");
-				conta.setBanco(scanner.next());
-
-				System.out.print("Numero: ");
-				conta.setNumero(scanner.nextInt());
-
-				System.out.print("Saldo: ");
-				conta.setSaldo(scanner.nextDouble());
-
-				System.out.print("Pessoa: ");
-				conta.setPessoa(daoPessoa.buscarPessoaPorId(scanner.nextInt()));
-				
-				daoConta.inserir(conta);
-				conta.mostrar();
+				inserirConta();
 				break;
 
 			case 3:
@@ -90,79 +225,11 @@ public class Main {
 				break;
 
 			case 5:
-				System.out.println("modificar Pessoa com id: ");
-				Pessoa p = daoPessoa.buscarPessoaPorId(scanner.nextInt());
-
-				System.out.print("Nome [" + p.getNome() + "]: ");
-				String novoNome = scanner.next();
-				if(!novoNome.isEmpty()) {
-					System.out.println(novoNome);
-					p.setNome(novoNome);
-				}
-
-				System.out.print("CPF [" + p.getCpf() + "]: ");
-				String novoCPF = scanner.next();
-				if(!novoCPF.isEmpty()) {
-					System.out.println(novoCPF);
-					p.setCpf(novoCPF);
-				}
-
-				System.out.print("Idade [" + p.getIdade() + "]: ");
-				String novaIdade = scanner.next();
-				if(!novaIdade.isEmpty()) {
-					System.out.println(novaIdade);
-					p.setIdade(Integer.parseInt(novaIdade));
-				}
-
-				System.out.print("Cidade [" + p.getCidade() + "]: ");
-				String novaCidade = scanner.next();
-				if(!novaCidade.isEmpty()) {
-					System.out.println(novaCidade);
-					p.setCidade(novaCidade);
-				}
-
-				daoPessoa.atualizarPessoa(p);
-				p = daoPessoa.buscarPessoaPorId(p.getId());
-				p.mostrar();
-
+				atualizarPessoa();
 				break;
 
 			case 6:
-				System.out.println("modificar Conta com id: ");
-				Conta c = daoConta.buscarContaPorId(scanner.nextInt());
-
-				System.out.print("Banco [" + c.getBanco() + "]: ");
-				String novoBanco = scanner.next();
-				if(!novoBanco.isEmpty()) {
-					System.out.println(novoBanco);
-					c.setBanco(novoBanco);
-				}
-
-				System.out.print("Numero [" + c.getNumero() + "]: ");
-				String novoNumero = scanner.next();
-				if(!novoNumero.isEmpty()) {
-					System.out.println(novoNumero);
-					c.setNumero(Integer.getInteger(novoNumero));
-				}
-
-				System.out.print("Saldo [" + c.getSaldo() + "]: ");
-				String novoSaldo = scanner.next();
-				if(!novoSaldo.isEmpty()) {
-					System.out.println(novoSaldo);
-					c.setSaldo(Double.parseDouble(novoSaldo));
-				}
-
-				System.out.print("Pessoa [ " + c.getPessoa().getId() +"]: ");
-				String novaPessoa = scanner.next();
-				if(!novaPessoa.isEmpty()) {
-					System.out.println(novaPessoa);
-					c.setPessoa(daoPessoa.buscarPessoaPorId(Integer.parseInt(novaPessoa)));
-				}
-
-				daoConta.atualizarConta(c);
-				c = daoConta.buscarContaPorId(c.getId());
-				c.mostrar();
-
+				atualizarConta();
 				break;
 				
 			case 7:
@@ -176,43 +243,11 @@ public class Main {
 				break;
 
 			case 9:
-				System.out.println("pesquisar Pessoa por: ");
-				System.out.println("[ 1 ] ID, [ 2 ] Nome, [ 3 ] CPF, [ 4 ] Cidade");
-				int filto_pessoa = scanner.nextInt();
-				switch(filto_pessoa) {
-				case 1:
-					break;
-
-				case 2:
-					break;
-
-				case 3:
-					break;
-
-				case 4:
-					break;
-				}
-
+				pesquisarPessoa();
 				break;
 
 			case 10:
-				System.out.println("pesquisar Conta por: ");
-				System.out.println("[ 1 ] ID, [ 2 ] Banco, [ 3 ] Numero, [ 4 ] ID_Pessoa");
-				int filto_conta = scanner.nextInt();
-				switch(filto_conta) {
-				case 1:
-					break;
-
-				case 2:
-					break;
-
-				case 3:
-					break;
-
-				case 4:
-					break;
-				}
-
+				pesquisarConta();
 				break;
 
 			default:
